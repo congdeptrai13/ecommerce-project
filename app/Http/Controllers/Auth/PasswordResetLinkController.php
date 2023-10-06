c8d3e6c88a62c65093844c7c1f388ab979aaa1b2
congdeptrai13 <cong102.201@gmail.com>
2023-10-06 19:26:24 +0700

    finish frontend template setup and mastering

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,9 +36,10 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        toastr()->success('Reset link has been seen to your mail');
         return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            ? back()->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,13 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -30,10 +28,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('admin/login',[AdminController::class,"login"])->name('admin.login');
+Route::get('admin/login', [AdminController::class, "login"])->name('admin.login');
 
 
 
 
 
 require __DIR__ . '/auth.php';
+
+
+Route::group(['middleware'=> ['auth','verified'], 'prefix' => 'user', 'as' =>'user.'],function(){
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
+
